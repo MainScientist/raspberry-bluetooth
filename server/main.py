b8b20e8e96971ec007426097364488231c3e2251
= <=>
2017-06-01 05:29:11 -0400

    scan

--- server/main.py
+++ server/main.py
@@ -8,6 +8,15 @@ def send(d):
     client_sock.send(json.dumps(d).encode("utf-8"))
 
 
+def scan():
+    global CELLLS
+    CELLS = {}
+    cells = list(Cell.all("wlan0"))
+    value = []
+    for c in cells:
+        CELLS[c.ssid.strip()] = c
+
+
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
@@ -24,12 +33,12 @@ advertise_service(server_sock, "SampleServer",
                  )
 
 
-
+CELLS = {}
+scan()
 while True:
     print("Waiting for connection on RFCOMM channel %d" % port)
     client_sock, client_info = server_sock.accept()
     print("Accepted connection from ", client_info)
-    CELLS = {}
     try:
         while True:
             data = client_sock.recv(1024)
@@ -38,21 +47,24 @@ while True:
             if command["action"] == "list":
                 cells = list(Cell.all("wlan0"))
                 value = []
-                for c in cells:
-                    CELLS[c.ssid] = c
-                    value.append(c.ssid)
+                for c in CELLS:
+                    value.append(c)
                 send({"value": value})
             elif command["action"] == "connect":
                 cell = CELLS[command["args"][0]]
                 scheme = Scheme.for_cell('wlan0', 'home', cell, command["args"][1])
                 scheme.save()
                 scheme.activate()
+                send({})
             elif command["action"] == "address":
                 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 s.connect(("8.8.8.8", 80))
                 address = s.getsockname()[0]
                 s.close()
                 send({"value": address})
+            elif command["action"] == "rescan":
+                scan()
+                send({})
             elif command["action"] == "exit":
                 break
             print("received [%s]" % data)
